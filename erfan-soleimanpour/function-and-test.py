9d76f2ca788db85cf6fd42cd08626ae870b1a1a5
erfanSoleimanpour <erfansol.ss@gmail.com>
2023-07-02 19:09:39 +0330

    function added

---
+++ erfan-soleimanpour/function-and-test.py
@@ -0,0 +1,8 @@
+def calculate_sum():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    result = num1 + num2
+    print("The sum of", num1, "and", num2, "is:", result)
+
+calculate_sum()
