5619e6ecf5fef953436716fa1deb2df81055db30
erfan-soleimanpour <erfansol.ss@gmail.com>
2023-07-09 16:01:09 +0330

    test added

--- erfan soleimanpour/test_main_9716081024.py
+++ erfan soleimanpour/test_main_9716081024.py
@@ -1,25 +1,5 @@
-class TestCalculateSum(unittest.TestCase):
+from main_9716081024 import calculate_sum
 
-    def test_calculate_sum(self):
-        # Test case 1: Positive numbers
-        self.assertEqual(calculate_sum(2, 3), 5)
-
-        # Test case 2: Negative numbers
-        self.assertEqual(calculate_sum(-5, -7), -12)
-
-        # Test case 3: Decimal numbers
-        self.assertEqual(calculate_sum(1.5, 2.5), 4.0)
-
-        # Test case 4: Zero
-        self.assertEqual(calculate_sum(0, 0), 0)
-
-        # Test case 5: Non-numeric input
-        with self.assertRaises(ValueError):
-            calculate_sum('a', 3)
-
-        # Test case 6: Missing input
-        with self.assertRaises(ValueError):
-            calculate_sum(5)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_name():
+    assert calculate_sum(5,6) == "11"
+    assert calculate_sum(5.1,2.5) == "7.6
